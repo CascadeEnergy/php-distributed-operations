9ae9bd5deae2f9df6308e5b8d168d185138fd483
Charles Berube <cberube@gmail.com>
2015-10-28 19:19:50 -0700

    Utility factory comments and consumer interface

--- src/Utility/UtilityFactoryInterface.php
+++ src/Utility/UtilityFactoryInterface.php
@@ -4,8 +4,23 @@ namespace CascadeEnergy\DistributedOperations\Utility;
 
 interface UtilityFactoryInterface
 {
+    /**
+     * @return CounterInterface
+     */
     public function createCounter();
+
+    /**
+     * @return ProviderInterface
+     */
     public function createProvider();
+
+    /**
+     * @return StorageInterface
+     */
     public function createStorage();
+
+    /**
+     * @return WaiterInterface
+     */
     public function createWaiter();
 }
