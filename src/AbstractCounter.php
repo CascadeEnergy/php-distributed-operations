782f87f7f09c5a400f93a0e8df61319f56f8c441
Charles Berube <cberube@gmail.com>
2015-10-28 19:00:05 -0700

    Add AbstractCounter utility class

---
+++ src/AbstractCounter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CascadeEnergy\DistributedOperations;
+
+abstract class AbstractCounter implements CounterInterface
+{
+    protected $batchId;
+
+    protected $disposition;
+
+    protected $state;
+
+    protected $type;
+
+    public function setBatchId($batchId)
+    {
+        $this->batchId = $batchId;
+    }
+
+    public function setDisposition($disposition)
+    {
+        $this->disposition = $disposition;
+    }
+
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
